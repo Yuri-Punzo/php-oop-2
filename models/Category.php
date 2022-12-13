04d167f75c073b797900cf2237bf4c416c0d577c
Yuri-Punzo <yuripunzo@hotmail.it>
2022-12-13 15:37:30 +0100

    cleaned. ex completed.

--- models/Category.php
+++ models/Category.php
@@ -18,19 +18,3 @@ class Category
 
     use construct1;
 }
-
-
-/* <?php
-trait message1 {
-  public function msg1() {
-    echo "OOP is fun! "; 
-  }
-}
-
-class Welcome {
-  use message1;
-}
-
-$obj = new Welcome();
-$obj->msg1();
-?> */
